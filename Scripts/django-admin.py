32ad9beab01c7ddb489687b30e712271084956a2
Jason Chan <jason27chan@gmail.com>
2017-05-19 16:45:02 -0400

    First commit

---
+++ Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\jason\onedrive\georgia tech\learning stuff\django\myenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
